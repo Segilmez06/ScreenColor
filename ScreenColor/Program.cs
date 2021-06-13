bf3bef0de66d724eec6d2c14ea7bdabab069d118
Sarp Eren EGILMEZ <2348.sarp.egilmez.2006@gmail.com>
2021-06-13 19:05:39 +0300

    Add files via upload

---
+++ ScreenColor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ScreenColor
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Process list = new Process();
+            list.StartInfo.FileName = "xrandr";
+            list.StartInfo.Arguments = "--listmonitors";
+
+            Process change = new Process();
+            change.StartInfo.FileName = "xrandr";
+
+            string selectedMon;
+            string selectedBri;
+
+            Console.WriteLine("================");
+            Console.WriteLine("ScreenColor v1.0");
+            Console.WriteLine("================");
+            Console.WriteLine();
+
+            list.Start();
+            list.WaitForExit();
+            Console.WriteLine();
+
+            Console.Write("Selected Monitor:");
+            selectedMon = Console.ReadLine();
+
+            Console.Write("Selected Brightness(Between 0.0 - 1.0):");
+            selectedBri = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Changing...");
+            change.StartInfo.Arguments = "--output " + selectedMon + " --brightness " + selectedBri;
+            change.Start();
+            change.WaitForExit();
+            Console.WriteLine("Changed.");
+            Console.WriteLine();
+
+            Console.Write("Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
